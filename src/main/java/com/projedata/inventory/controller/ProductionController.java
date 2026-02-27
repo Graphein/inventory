67c35ccc05009ba5e79c2f943a24275f97e8e8c7
Eduardo Leli <grpheinleli@gmail.com>
2026-02-27 15:28:48 -0300

    feat: create base controllers for product, raw material and productio

---
+++ src/main/java/com/projedata/inventory/controller/ProductionController.java
@@ -0,0 +1,30 @@
+package com.projedata.inventory.controller;
+
+import com.projedata.inventory.entity.Production;
+import com.projedata.inventory.service.ProductionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productions")
+@CrossOrigin("*")
+public class ProductionController {
+
+    private final ProductionService productionService;
+
+    public ProductionController(ProductionService productionService) {
+        this.productionService = productionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Production>> findAll() {
+        return ResponseEntity.ok(productionService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Production> create(@RequestBody Production production) {
+        return ResponseEntity.ok(productionService.createProduction(production));
+    }
+}
